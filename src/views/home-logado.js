8416ae062f2cc418266889da640adac446776ad1
Alessandro Rodrigues <47608938+eualessandro@users.noreply.github.com>
2023-06-20 22:24:51 -0300

    Commit from TeleportHQ

---
+++ src/views/home-logado.js
@@ -0,0 +1,185 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home-logado.css'
+
+const HomeLogado = (props) => {
+  return (
+    <div className="home-logado-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="home-logado-home-logado">
+        <div className="home-logado-frame629050">
+          <header className="home-logado-header">
+            <div className="home-logado-logo">
+              <div className="home-logado-logo1">
+                <img
+                  alt="Clipboard20deAbrilde2023s16081I581"
+                  src="/external/clipboard20deabrilde2023s16081i581-uz56-200h.png"
+                  className="home-logado-clipboard20de-abrilde2023s16081"
+                />
+                <img
+                  alt="Clipboard20deAbrilde2023s16082I581"
+                  src="/external/clipboard20deabrilde2023s16082i581-02c9-200h.png"
+                  className="home-logado-clipboard20de-abrilde2023s16082"
+                />
+              </div>
+              <div className="home-logado-titulo">
+                <span className="home-logado-text">
+                  <span>Portal</span>
+                </span>
+                <span className="home-logado-text2">
+                  <span>Contribuinte</span>
+                </span>
+              </div>
+            </div>
+            <nav className="home-logado-user">
+              <img
+                alt="UsersRightVariant4I581"
+                src="/external/usersrightvariant4i581-wgj4.svg"
+                className="home-logado-users-right-variant4"
+              />
+              <img
+                alt="UsersRightDefaultI581"
+                src="/external/usersrightdefaulti581-sff8.svg"
+                className="home-logado-users-right-default"
+              />
+              <img
+                alt="UsersRightDefaultI581"
+                src="/external/usersrightdefaulti581-goh.svg"
+                className="home-logado-users-right-default1"
+              />
+            </nav>
+          </header>
+        </div>
+        <div className="home-logado-wrapper">
+          <div className="home-logado-frame629056">
+            <div className="home-logado-menu">
+              <div className="home-logado-top">
+                <div className="home-logado-profile">
+                  <div className="home-logado-avatar">
+                    <span className="home-logado-text4">=</span>
+                  </div>
+                </div>
+                <div className="home-logado-seach">
+                  <img
+                    alt="IconsI581"
+                    src="/external/iconsi581-l9b.svg"
+                    className="home-logado-icons"
+                  />
+                </div>
+                <div className="home-logado-list-item">
+                  <div className="home-logado-list-manu">
+                    <img
+                      alt="IconsI581"
+                      src="/external/iconsi581-9oat.svg"
+                      className="home-logado-icons1"
+                    />
+                  </div>
+                  <div className="home-logado-list-manu1">
+                    <div className="home-logado-icons2">
+                      <img
+                        alt="Rectangle2877I581"
+                        src="/external/rectangle2877i581-m9o4-200h.png"
+                        className="home-logado-rectangle2877"
+                      />
+                      <img
+                        alt="Vector1325I581"
+                        src="/external/vector1325i581-h4zn.svg"
+                        className="home-logado-vector1325"
+                      />
+                      <img
+                        alt="Vector1326I581"
+                        src="/external/vector1326i581-b0lo.svg"
+                        className="home-logado-vector1326"
+                      />
+                      <img
+                        alt="Vector1327I581"
+                        src="/external/vector1327i581-ziq.svg"
+                        className="home-logado-vector1327"
+                      />
+                    </div>
+                  </div>
+                  <div className="home-logado-list-manu2">
+                    <img
+                      alt="IconsI581"
+                      src="/external/iconsi581-0fmm.svg"
+                      className="home-logado-icons3"
+                    />
+                  </div>
+                  <div className="home-logado-list-manu3">
+                    <img
+                      alt="IconsI581"
+                      src="/external/iconsi581-4z5.svg"
+                      className="home-logado-icons4"
+                    />
+                  </div>
+                  <div className="home-logado-list-manu4">
+                    <img
+                      alt="IconsI581"
+                      src="/external/iconsi581-xpqi.svg"
+                      className="home-logado-icons5"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="home-logado-bottom">
+                <div className="home-logado-list-manu5">
+                  <img
+                    alt="IconsI581"
+                    src="/external/iconsi581-6x1o.svg"
+                    className="home-logado-icons6"
+                  />
+                </div>
+                <div className="home-logado-switchtoggle">
+                  <img
+                    alt="BaseI581"
+                    src="/external/basei581-75sm-200h.png"
+                    className="home-logado-base"
+                  />
+                  <img
+                    alt="KnobI581"
+                    src="/external/knobi581-g8uu-200h.png"
+                    className="home-logado-knob"
+                  />
+                  <img
+                    alt="IconsI581"
+                    src="/external/iconsi581-a9e9.svg"
+                    className="home-logado-icons7"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="home-logado-container1">
+            <div className="home-logado-topo-pages">
+              <div className="home-logado-search">
+                <span className="home-logado-text5">
+                  <span>Terça-feira, 7 de março de 2023</span>
+                </span>
+              </div>
+              <div className="home-logado-search1">
+                <div className="home-logado-input">
+                  <div className="home-logado-content">
+                    <img
+                      alt="searchlg5715"
+                      src="/external/searchlg5715-aemd.svg"
+                      className="home-logado-searchlg"
+                    />
+                    <span className="home-logado-text7 TextmdNormal">
+                      <span>Pesquisar</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomeLogado
